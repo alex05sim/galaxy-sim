f1e6c9ba5aa9aeda66a20ff5cc613c37e3559205
Alex <alex05sim@berkeley.edu>
2025-05-29 21:19:36 -0400

    fixes still need gpu cuda optimization

--- src/galaxy_sim/main.py
+++ src/galaxy_sim/main.py
@@ -4,17 +4,34 @@ from galaxy_sim.view_controller import BodyInteractor
 from galaxy_sim.viewer import OrbitViewer3D
 from galaxy_sim.solar_system import load_solar_system
 
+
 # Setup system
 bodies = load_solar_system()
 update_accelerations(bodies)
 
 # Viewer
 viewer = OrbitViewer3D(bodies)
+viewer.time_multiplier = 1.0
+viewer.base_dt = 60*60# 1 second per step
+
 interactor = BodyInteractor(viewer)
+
 
 # Hook up simulation update
 def simulate(event):
-    velocity_verlet_step(bodies, dt=60 * 60)
+    viewer.steps_to_run += viewer.time_multiplier
+
+    # Cap how many we simulate in one frame (avoid freezing)
+    max_steps_per_frame = 1000
+
+    steps_this_frame = min(int(viewer.steps_to_run), max_steps_per_frame)
+    for _ in range(steps_this_frame):
+        velocity_verlet_step(bodies, dt=viewer.base_dt)
+        viewer.sim_time += viewer.base_dt
+        viewer.steps_to_run -= 1
+
+
+
 
 viewer.timer.connect(simulate)
 viewer.run()
